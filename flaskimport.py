bd0b691c55bd0baf750ac58ad474586ccb9b90a7
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2025-07-15 14:34:24 +1200

    moved nav bar to layout.html and cleaned up code

--- flaskimport.py
+++ flaskimport.py
@@ -9,6 +9,11 @@ def home():
     return render_template("home.html", title="Home")
 
 
+@app.route('/athletes')
+def athletes():
+    return render_template('athletes.html')
+
+
 if __name__ == '__main__':
     # TAKE OUT BEFORE SUBMIT
     app.run(debug=True)
